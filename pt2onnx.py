39ac0a71bc2f3fed216ea79ae53658c0ec5ee10b
UltralyticsAssistant <web@ultralytics.com>
2026-07-06 08:48:52 +0000

    Auto-format by https://ultralytics.com/actions

--- pt2onnx.py
+++ pt2onnx.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO(r"D:\xinyu\ultralytics-main\runs\pose\tip_pose扩增_v8pose\200e_0608_wl_02\weights\best.pt")  # load a custom-trained model
+model = YOLO(
+    r"D:\xinyu\ultralytics-main\runs\pose\tip_pose扩增_v8pose\200e_0608_wl_02\weights\best.pt"
+)  # load a custom-trained model
 
 # Export the model
 model.export(format="onnx", imgsz=480, half=False)
